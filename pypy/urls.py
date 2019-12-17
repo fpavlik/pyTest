ec465c0e063af652d434831489c6b8217fffbf13
Filipp Pavlik <fpavlik@mail.ru>
2019-12-17 18:04:33 +0300

    fixes

--- pypy/urls.py
+++ pypy/urls.py
@@ -16,14 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
-from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+    path('modeling/', include('modeling.urls')),
+    path('', RedirectView.as_view(url='/modeling/', permanent=False)),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
